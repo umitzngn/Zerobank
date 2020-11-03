293149b2ac87c83e05e305053525cae7c6c9983e
umitzngn <60893281+umitzngn@users.noreply.github.com>
2020-11-03 17:05:40 +0100

    @smoke tags added

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@smoke"
 )
 
 public class CukesRunner {
