c2f904458dd90de5cac1040ebd81eb8bba72e4e8
umitzngn <60893281+umitzngn@users.noreply.github.com>
2020-12-04 22:01:08 +0100

    CukesRunner

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "wip"
 )
 
 public class CukesRunner {
